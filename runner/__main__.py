34a71d3ac44dc7975982014b6c2934b7bfb6de0d
raphlcc <ra_carvalho@id.uff.br>
2025-05-16 14:51:09 -0300

    fix: add sequential program execution to cspeed up

--- runner/__main__.py
+++ runner/__main__.py
@@ -1,6 +1,7 @@
 import sys
 
 from run_mpi import RunMPIPrimos
+from run_sequential import RunSequential
 from file_manager import FileManager
 
 if __name__ == "__main__":
@@ -21,32 +22,34 @@ if __name__ == "__main__":
 
     run_primos = RunMPIPrimos(folder)
     file_manager = FileManager(folder)
+    run_sequential = RunSequential("./sequential/primos.c")
 
-    #Execute with 1, 2 and 4 processes
-    if folder == "bag_of_tasks":
-        run_primos_1 = RunMPIPrimos("naive")
-        r1 = run_primos_1.run_all(num_procs=1, exec_number=exec_number, args=['1000000'])
-    else:
-        r1 = run_primos.run_all(num_procs=1, exec_number=exec_number, args=['1000000'])
+    #Execute with 2, 4 and 8 processes
+
+    r1_mean = run_sequential.get_mean(exec_number, args=[1000000])
 
     r2 = run_primos.run_all(num_procs=2, exec_number=exec_number, args=['1000000'])
     r4 = run_primos.run_all(num_procs=4, exec_number=exec_number, args=['1000000'])
-
-    print(r1, r2, r4)
+    r8 = run_primos.run_all(num_procs=8, exec_number=exec_number, args=['1000000'])
 
     names = file_manager.list_files(show_path=True)
     for name in names:
-        speed_up_2_procs = r1[name] / r2[name]
+        speed_up_2_procs = r1_mean / r2[name]
         efficiency_2_procs  =speed_up_2_procs/2
 
-        speed_up_4_procs = r1[name] / r4[name]
+        speed_up_4_procs = r1_mean / r4[name]
         efficiency_4_procs  =speed_up_4_procs/2
+
+        speed_up_8_procs = r1_mean / r8[name]
+        efficiency_8_procs  =speed_up_8_procs/2
 
         content = [
             f"Speed-Up com 2 processos: {speed_up_2_procs}",
             f"Eficiência com 2 processos: {efficiency_2_procs}\n"
             f"Speed-Up com 4 processos: {speed_up_4_procs}",
-            f"Eficiência com 4 processos: {efficiency_4_procs}",
+            f"Eficiência com 4 processos: {efficiency_4_procs}\n",
+            f"Speed-Up com 8 processos: {speed_up_8_procs}",
+            f"Eficiência com 8 processos: {efficiency_8_procs}",
         ]
 
         file_manager.write_generator(f"relatorio/{name}/avaliacao.txt", content)
---
+++ runner/run_sequential.py
@@ -0,0 +1,113 @@
+import subprocess
+import os
+
+from file_manager import FileManager
+
+class RunSequential:
+    """
+    A utility class to compile and run the sequential version.
+    """
+
+    __path: str
+
+    def __init__(self, path: str):
+        self.__path = path
+        self.compile()
+
+    def compile(self) -> None:
+        """
+        Compiles a C program using gcc.
+        """
+        if not os.path.isfile(self.__path):
+            raise RuntimeError(f"Error: File '{self.__path}' not found.")
+        
+        output, extension = os.path.splitext(self.__path)
+
+        if extension != ".c":
+            return
+
+        try:
+            subprocess.run(
+                ['gcc', self.__path, '-o', output, "-lm"],
+                capture_output=True,
+                check=True  # Raises CalledProcessError on failure
+            )
+
+            self.__path = output
+
+            print(f"File {self.__path} compiled with success.")
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(f"Program {self.__path} does not compile.")
+        except FileNotFoundError:
+            raise RuntimeError("Error: 'mpicc' not found. Make sure it is "
+                               "installed and available in.")
+        
+    def run_program(self, args=None) -> float:
+        """
+        Runs a compiled C program.
+
+        Parameters:
+            args (list): Optional list of arguments to pass to the executable
+
+        Returns:
+            float: The execution time of the program.
+        """
+        if not os.path.isfile(self.__path):
+            return f"Error: Executable '{self.__path}' not found."
+
+        command = [self.__path]
+        
+        if args:
+            command.extend(args)
+
+        try:
+            result = subprocess.run(
+                command,
+                capture_output=True,
+                text=True,
+                check=True
+            )
+            _, exec_time_text, _ = result.stdout.split("\n")
+            _, time_value = exec_time_text.split(": ")
+            
+            return float(time_value)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(f"Execution error:\n{e.stderr}")
+        except FileNotFoundError:
+            raise RuntimeError("Error: 'mpirun' not found. Make sure MPI is "
+                               "installed and available in the system PATH.")
+
+    def get_mean(self, n_times: int, **kwargs) -> float:
+        """
+        Execute the program `n_times` times, and returns the average execution
+        time.
+
+        Args:
+            n_times (int): The number of executions to be applied.
+            *kwargs: the program args.
+
+        Returns:
+            float: the average execution time.
+
+        Side Effects:
+            Writes all times and the mean in a file.
+        """
+        executions = []
+
+        for _ in range(n_times):
+            time = self.run_program(**kwargs)
+            executions.append(time)
+
+        mean = sum(executions) / n_times
+
+        content = list(map(lambda time: str(time), executions))
+        content += [f"\nO tempo médio foi: {mean}"]
+
+        fm = FileManager("sequential")
+        fm.write_generator("relatorio/sequential/primos.txt", content)
+
+
+if __name__ == "__main__":
+    run = RunSequential("./sequential/primos.c")
+    mean = run.get_mean(n_times=30, args=["1000000"])
+    print(mean)
